99be7f29149f36ab4d3c62768ece3f9e6a1016d7
Delpedro <delpeter@gmail.com>
2026-08-09 16:30:22 +0100

    fix: PWA start_url/scope and cached asset paths were absolute, 404ing on GitHub Pages subpath

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
 const CACHE_NAME = 'dlog-v1';
 const APP_SHELL = [
-  '/',
-  '/index.html',
-  '/css/style.css',
-  '/js/app.js',
-  '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png',
+  './',
+  './index.html',
+  './css/style.css',
+  './js/app.js',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
 ];
 
 self.addEventListener('install', (event) => {
